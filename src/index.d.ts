fbefb0592c9d71144a62a60613f2ca0ec952c9d2
mxyhi <2528988582@qq.com>
2024-02-19 16:56:23 +0800

    ⚡️ "1.1.0"

--- src/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare const createPromise: <T>(_?: T) => {
-    promise: any;
-    resolve: (value: T | PromiseLike<T>) => void;
-    reject: (reason?: any) => void;
-};
-export { createPromise };
-export default createPromise;
